ef092d6623760cf07b35ac056af0d0af483bae4d
kuibono@gmail.com <kuibono@gmail.com@6935b428-df36-908e-fed2-a42483d1cdbb>
2011-10-06 11:50:53 +0000

    git-svn-id: https://aspnet-voodoo.googlecode.com/svn/trunk@24 6935b428-df36-908e-fed2-a42483d1cdbb

---
+++ other/Messanger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Voodoo
+{
+    public class Messanger
+    {
+        public string MessangerNumber { get; set; }
+
+        public e_Messanger MessagnerType{get;set;}
+    }
+}

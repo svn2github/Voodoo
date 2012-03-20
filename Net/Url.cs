381c3cc8254c99edcc43884815c6cfa11eb09ec0
kuibono@gmail.com <kuibono@gmail.com@6935b428-df36-908e-fed2-a42483d1cdbb>
2012-03-20 13:44:15 +0000

    git-svn-id: https://aspnet-voodoo.googlecode.com/svn/trunk@68 6935b428-df36-908e-fed2-a42483d1cdbb

--- Net/Url.cs
+++ Net/Url.cs
@@ -1187,6 +1187,75 @@ namespace Voodoo.Net
 
         }
         #endregion
+
+        #region WebClient上传文件至服务器
+        /// <summary>
+        /// WebClient上传文件至服务器
+        /// </summary>
+        /// <param name="fileNamePath">文件名，全路径格式</param>
+        /// <param name="uriString">服务器文件夹路径</param>
+        /// <param name="IsAutoRename">是否自动按照时间重命名</param>
+        public static void UpLoadFile(string fileNamePath, string uriString, bool IsAutoRename)
+        {
+            string fileName = fileNamePath.Substring(fileNamePath.LastIndexOf("\\") + 1);
+            string NewFileName = fileName;
+            if (IsAutoRename)
+            {
+                NewFileName = DateTime.Now.ToString("yyMMddhhmmss") + DateTime.Now.Millisecond.ToString() + fileNamePath.Substring(fileNamePath.LastIndexOf("."));
+            }
+
+            string fileNameExt = fileName.Substring(fileName.LastIndexOf(".") + 1);
+            if (uriString.EndsWith("/") == false) uriString = uriString + "/";
+
+            uriString = uriString + NewFileName;
+            //log.AddLog(uriString, "Log");
+            //log.AddLog(fileNamePath, "Log");
+            /**/
+            /// 创建WebClient实例
+            WebClient myWebClient = new WebClient();
+            myWebClient.Credentials = CredentialCache.DefaultCredentials;
+            // 要上传的文件
+            FileStream fs = new FileStream(fileNamePath, FileMode.Open, FileAccess.Read);
+            //FileStream fs = OpenFile();
+            BinaryReader r = new BinaryReader(fs);
+            byte[] postArray = r.ReadBytes((int)fs.Length);
+            Stream postStream = myWebClient.OpenWrite(uriString, "PUT");
+
+
+            try
+            {
+
+                //使用UploadFile方法可以用下面的格式
+                //myWebClient.UploadFile(uriString,"PUT",fileNamePath);
+
+
+                if (postStream.CanWrite)
+                {
+                    postStream.Write(postArray, 0, postArray.Length);
+                    postStream.Close();
+                    fs.Dispose();
+                }
+                else
+                {
+                    postStream.Close();
+                    fs.Dispose();
+                }
+
+            }
+            catch (Exception err)
+            {
+                postStream.Close();
+                fs.Dispose();
+                throw err;
+            }
+            finally
+            {
+                postStream.Close();
+                fs.Dispose();
+            }
+        }
+        #endregion
+
     }
 
     #region 用户抓取操作返回数据类，如Cookie，HTML代码等
